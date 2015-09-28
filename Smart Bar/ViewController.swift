82b8f670d90567fe64e5bd367910d9355e1d1414
Javier Rivarola <javierrivarola@tacuaratech.com>
2015-09-28 17:47:18 -0400

    Initial Commit

---
+++ Smart Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart Bar
+//
+//  Created by Javier Rivarola on 9/28/15.
+//  Copyright © 2015 Javier Rivarola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
